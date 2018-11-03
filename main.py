07f250efa61f1d8050a99d0083a727b7778fd0be
Yukihiro Takamoto <organowhatthehell1974@gmail.com>
2018-11-03 19:57:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import datetime
-import urllib3
+#import urllib3
 import requests
 from requests_oauthlib import OAuth1
 import json
@@ -10,13 +10,10 @@ import codecs
 import tkinter
 import sys
 import smtplib
-from email.mime.text import MIMEText
-from email.parser import Parser
 from email.message import EmailMessage
-
+import winsound
 
 today = datetime.date.today()
-
 print(today.year, "year", today.month, "month", today.day, "day")
 
 api_key = ""
@@ -28,65 +25,68 @@ AUTH = OAuth1(api_key, api_secret, access_token, access_secret)
 
 url = "https://stream.twitter.com/1.1/statuses/filter.json"
 
-
-followersIdGet = 'https://api.twitter.com/1.1/followers/ids.json?screen_name='
-
 def getUserId(scName):
     countN = '&count=1'
     urlIdGet = 'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=' + scName + countN
     userIdRes = requests.get(urlIdGet, auth=AUTH)
     userIdJson = json.loads(userIdRes.text)
     for searchRes in userIdJson:
-        print(searchRes['user']['id'])
+        scNameID = searchRes['user']['id']
+        print(scName + ' ID is : '  +  str(scNameID))
+        sys.exit('Twitter user ID Search done.')
 
-#getUserId('KCNAWatch')
+#getUserId('AbeShinzo') # Input twitter screen name. Example: @realDonaldTrump. Input 'realDonaldTrump'
 
-def emailTo(tweetSrc):
+def emailTo(tweetSrc, ScreenName, timeAt):
 
-    gmailUserName = '*********@gmail.com'
-    gmailPassword = 'your_password'
-    targetEmail = 'target_mail@******.com'
-
-    smtpServer = smtplib.SMTP('smtp.gmail.com', 587) #
-    #smtpServer.connect('smtp.gmail.com', 587) #Connect to smtp.gmail.com on port 465, if you’re using SSL. (Connect on port 587 if you’re using TLS.)
-    smtpServer.ehlo()
-    smtpServer.starttls()
-   #smtpServer.ehlo()
-
-    mailHeaders = Parser().parsestr('From: <@gmail.com>\n'
-                                'To: <@outlook.com>\n'
-                                'Subject: Test message\n'
-                                '\n'
-                                'Body would go here\n')
+    gmailUserName = '@gmail.com'
+    gmailPassword = ''
+    targetEmail = '@outlook.com'
 
     message = EmailMessage()
     message.set_content(tweetSrc)
-    message['subject'] = 'Test send'
+    message['subject'] = ScreenName + ' tweet at ' + timeAt
     message['From'] = gmailUserName
     message['To'] = targetEmail
 
-    smtpServer.login(gmailUserName, gmailPassword)
-    #smtpServer.sendmail(gmailUserName, targetEmail, tweetSrc)
+    smtpServer = smtplib.SMTP('smtp.gmail.com', 587)
+    #Connect to smtp.gmail.com on port 465, if you’re using SSL. (Connect on port 587 if you’re using TLS.)
+    smtpServer.ehlo()
+    smtpServer.starttls()
+
+    try:
+        smtpServer.login(gmailUserName, gmailPassword)
+    except smtplib.SMTPAuthenticationError:
+        print('Sorry, We can not access your google account.Because, this is not secure application for google policy.')
+        print('Allow less secure apps at the site of google account, \"Log-in & Security\".')
+        print('If it is not, your gmail ID or Password is incorrect.\n')
+        return False
+    except:
+        return False
+
     smtpServer.send_message(message)
     smtpServer.quit()
-
+    message.clear()
+    return True
 
 trumpID = 25073877
 civilAirID = 1064031036
 rt_comId = 64643056
 kcnaId = 612926882
 
-searchTypy = u'follow'
-searchWord = ''
+searchTypyFollow = 'follow'
+searchTypyTrack = 'track'
+searchWord = u''
 followUserID = [rt_comId, trumpID, civilAirID, kcnaId]
 
-resp = requests.post(url, auth=AUTH,  stream=True, data={searchTypy: followUserID})
+resp = requests.post(url, auth=AUTH,  stream=True, data={searchTypyFollow: followUserID})
 
 for lineFeed in resp.iter_lines():
 
+    decoded_line = lineFeed.decode('utf-8')
     while True:
         try:
-            lineFeed = json.loads(lineFeed)
+            lineFeed = json.loads(decoded_line)
         except ValueError:
             #print('Value Error')
             break
@@ -99,14 +99,17 @@ for lineFeed in resp.iter_lines():
                userScName = lineFeed['user']['screen_name']
                tweetText = lineFeed['text']
                print(tweetAt)
-               print(userScName + '   '+ tweetText + '\n')
-               emailTo(tweetText)
-               #print(userID)
+               print(userScName + ': '+ tweetText + '\n')
+
+               try:
+                   winsound.Beep(900, 1000) # Beep(frequency, millisecond). The frequency parameter must be in the range 37 through 32767.
+               except RuntimeError:
+                   break
+
+               emailTo(tweetText, userScName, tweetAt)
+
            break
 
-        except KeyError:
-            print('Key Error')
-            break
-        except TypeError:
-            print('Type Error')
+        except(KeyError, TypeError):
+            #print('Key Error')
             break
