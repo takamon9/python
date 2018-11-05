d0351482bce6901855ec7afd98732ac62bcbd14d
Yukihiro Takamoto <organowhatthehell1974@gmail.com>
2018-11-05 23:07:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,28 +2,17 @@
 # -*- coding: utf-8 -*-
 
 import datetime
-#import urllib3
 import requests
 from requests_oauthlib import OAuth1
 import json
 import codecs
-import tkinter
+import tkinter as tk
+import  tkinter.messagebox
 import sys
 import smtplib
 from email.message import EmailMessage
 import winsound
-
-today = datetime.date.today()
-print(today.year, "year", today.month, "month", today.day, "day")
-
-api_key = ""
-api_secret = ""
-access_token = ""
-access_secret = ""
-
-AUTH = OAuth1(api_key, api_secret, access_token, access_secret)
-
-url = "https://stream.twitter.com/1.1/statuses/filter.json"
+import threading
 
 def getUserId(scName):
     countN = '&count=1'
@@ -35,13 +24,12 @@ def getUserId(scName):
         print(scName + ' ID is : '  +  str(scNameID))
         sys.exit('Twitter user ID Search done.')
 
-#getUserId('AbeShinzo') # Input twitter screen name. Example: @realDonaldTrump. Input 'realDonaldTrump'
 
 def emailTo(tweetSrc, ScreenName, timeAt):
 
     gmailUserName = '@gmail.com'
     gmailPassword = ''
-    targetEmail = '@outlook.com'
+    targetEmail = 'A@******.com'
 
     message = EmailMessage()
     message.set_content(tweetSrc)
@@ -69,47 +57,106 @@ def emailTo(tweetSrc, ScreenName, timeAt):
     message.clear()
     return True
 
-trumpID = 25073877
-civilAirID = 1064031036
-rt_comId = 64643056
-kcnaId = 612926882
 
-searchTypyFollow = 'follow'
-searchTypyTrack = 'track'
-searchWord = u''
-followUserID = [rt_comId, trumpID, civilAirID, kcnaId]
 
-resp = requests.post(url, auth=AUTH,  stream=True, data={searchTypyFollow: followUserID})
+class gui(tk.Frame):
+    def __init__(self,master=None):
+        super().__init__(master)
+        self.master = master
+        self.createWidgets()
 
-for lineFeed in resp.iter_lines():
+    def findTwitterThread(self):
+        threEvent = threading.Event()
+        threEvent.set()
+        thre = threading.Thread(target=self.method)
+        thre.start()
 
-    decoded_line = lineFeed.decode('utf-8')
-    while True:
-        try:
-            lineFeed = json.loads(decoded_line)
-        except ValueError:
-            #print('Value Error')
-            break
+    def callEnd(self):
+        if tk.messagebox.askyesno('Question?', 'Are you sure you want to quit the program?'):
+            root.destroy()
+            exit(0)
 
-        try:
-           userID = lineFeed['user']['id']
-           tweetAt = lineFeed['created_at']
+    def createWidgets(self):
+        self.button1 = tk.Button()
+        self.button1['text'] = 'Start'
+        self.button1['command'] = self.findTwitterThread
+        self.button1.pack(side='bottom')
+        self.button2 = tk.Button(text='Quit', command=self.callEnd).place(x=600,y=450)
+        self.textBox = tk.Text()
+        self.textBox['wrap'] = tk.WORD
+        self.textBox.place(x=20,y=30)
 
-           if userID in followUserID:
-               userScName = lineFeed['user']['screen_name']
-               tweetText = lineFeed['text']
-               print(tweetAt)
-               print(userScName + ': '+ tweetText + '\n')
+    def method(self):
+        today = datetime.date.today()
+        print(today.year, "year", today.month, "month", today.day, "day")
+        #getUserId('AbeShinzo') # Input twitter screen name. Example: @realDonaldTrump. Input 'realDonaldTrump'
+        self.textBox.insert('1.0','Waiting tweet now...\n')
+        api_key = ""
+        api_secret = ""
+        access_token = "2"
+        access_secret = ""
 
-               try:
-                   winsound.Beep(900, 1000) # Beep(frequency, millisecond). The frequency parameter must be in the range 37 through 32767.
-               except RuntimeError:
-                   break
+        AUTH = OAuth1(api_key, api_secret, access_token, access_secret)
 
-               emailTo(tweetText, userScName, tweetAt)
+        url = "https://stream.twitter.com/1.1/statuses/filter.json"
 
-           break
+        trumpID = 25073877
+        civilAirID = 1064031036
+        rt_comId = 64643056
+        kcnaId = 612926882
 
-        except(KeyError, TypeError):
-            #print('Key Error')
-            break
+        follewID = [trumpID, civilAirID, rt_comId, kcnaId]
+
+        searchTypyFollow = 'follow'
+        searchTypyTrack = 'track'
+        searchWord = u''
+
+        resp = requests.post(url, auth=AUTH,  stream=True, data={searchTypyFollow: follewID})
+
+        for lineFeed in resp.iter_lines():
+
+            decoded_line = lineFeed.decode('utf-8')
+            while True:
+                try:
+                    lineFeed = json.loads(decoded_line)
+
+                except ValueError:
+                    #print('Value Error')
+                    break
+                try:
+                    userID = lineFeed['user']['id']
+                    tweetAt = lineFeed['created_at']
+                    if userID in follewID:
+                        userScName = lineFeed['user']['screen_name']
+                        tweetText = lineFeed['text']
+                        contentTweet = userScName + ': '+ tweetText + '\n'
+                        print(tweetAt)
+                        print(contentTweet)
+                        self.textBox.insert('1.0', '\n\n')
+                        self.textBox.insert('1.0', contentTweet)
+                        self.textBox.insert('1.0', '\n\n')
+                        self.textBox.insert('1.0', tweetAt)
+
+                        try:
+                            winsound.Beep(900,1000)  # Beep(frequency, millisecond). The frequency parameter must be in the range 37 through 32767.
+                        except RuntimeError:
+                            break
+
+                        mailBool = emailTo(tweetText, userScName, tweetAt)
+                        if mailBool is False:
+                            print('We can not send the E-mail.\n')
+
+                    break
+
+                except(KeyError, TypeError):
+                    # print('Key Error')
+                    break
+root = tk.Tk()
+root.geometry('700x500')
+root.title('twitter stream GUI')
+app = gui(master=root)
+app.mainloop()
+
+#if __name__ == '__main__':
+
+
