d39a9471eb7ae4cfacdb84d63519c54487d38c53
Yukihiro Takamoto <organowhatthehell1974@gmail.com>
2018-11-07 21:48:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,22 +14,11 @@ from email.message import EmailMessage
 import winsound
 import threading
 
-def getUserId(scName):
-    countN = '&count=1'
-    urlIdGet = 'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=' + scName + countN
-    userIdRes = requests.get(urlIdGet, auth=AUTH)
-    userIdJson = json.loads(userIdRes.text)
-    for searchRes in userIdJson:
-        scNameID = searchRes['user']['id']
-        print(scName + ' ID is : '  +  str(scNameID))
-        sys.exit('Twitter user ID Search done.')
-
-
 def emailTo(tweetSrc, ScreenName, timeAt):
 
     gmailUserName = '@gmail.com'
     gmailPassword = ''
-    targetEmail = 'A@******.com'
+    targetEmail = '.com'
 
     message = EmailMessage()
     message.set_content(tweetSrc)
@@ -65,11 +54,19 @@ class gui(tk.Frame):
         self.master = master
         self.createWidgets()
 
+        api_key = ""
+        api_secret = ""
+        access_token = ""
+        access_secret = ""
+        self.AUTH = OAuth1(api_key, api_secret, access_token, access_secret)
+
     def findTwitterThread(self):
         threEvent = threading.Event()
         threEvent.set()
+        self.button1['state'] = tk.DISABLED
         thre = threading.Thread(target=self.method)
         thre.start()
+
 
     def callEnd(self):
         if tk.messagebox.askyesno('Question?', 'Are you sure you want to quit the program?'):
@@ -77,26 +74,34 @@ class gui(tk.Frame):
             exit(0)
 
     def createWidgets(self):
+        self.button2 = tk.Button(text='Quit', command=self.callEnd, pady=5, padx=20).pack(side=tk.BOTTOM, fill=tk.X)
         self.button1 = tk.Button()
         self.button1['text'] = 'Start'
         self.button1['command'] = self.findTwitterThread
-        self.button1.pack(side='bottom')
-        self.button2 = tk.Button(text='Quit', command=self.callEnd).place(x=600,y=450)
+        self.button1['padx'] = 20
+        self.button1['pady'] = 5
+        self.button1.pack(side=tk.BOTTOM, fill=tk.X)
         self.textBox = tk.Text()
         self.textBox['wrap'] = tk.WORD
-        self.textBox.place(x=20,y=30)
+       # self.textBox['font'] = self.helv14
+        self.textBox.pack(side=tk.BOTTOM, fill=tk.X)
+
+    def getUserId(scName):
+        countN = '&count=1'
+        urlIdGet = 'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=' + scName + countN
+        userIdRes = requests.get(urlIdGet, auth=self.AUTH)
+        userIdJson = json.loads(userIdRes.text)
+        for searchRes in userIdJson:
+            scNameID = searchRes['user']['id']
+            print(scName + ' ID is : ' + str(scNameID))
+            sys.exit('Twitter user ID Search done.')
 
     def method(self):
         today = datetime.date.today()
         print(today.year, "year", today.month, "month", today.day, "day")
         #getUserId('AbeShinzo') # Input twitter screen name. Example: @realDonaldTrump. Input 'realDonaldTrump'
-        self.textBox.insert('1.0','Waiting tweet now...\n')
-        api_key = ""
-        api_secret = ""
-        access_token = "2"
-        access_secret = ""
+        self.textBox.insert('1.0','Waiting tweet now...\n\n')
 
-        AUTH = OAuth1(api_key, api_secret, access_token, access_secret)
 
         url = "https://stream.twitter.com/1.1/statuses/filter.json"
 
@@ -104,14 +109,15 @@ class gui(tk.Frame):
         civilAirID = 1064031036
         rt_comId = 64643056
         kcnaId = 612926882
+        zeroHedge = 18856867
 
-        follewID = [trumpID, civilAirID, rt_comId, kcnaId]
+        follewID = [trumpID, civilAirID, rt_comId, kcnaId, zeroHedge]
 
         searchTypyFollow = 'follow'
         searchTypyTrack = 'track'
         searchWord = u''
 
-        resp = requests.post(url, auth=AUTH,  stream=True, data={searchTypyFollow: follewID})
+        resp = requests.post(url, auth=self.AUTH,  stream=True, data={searchTypyFollow: follewID})
 
         for lineFeed in resp.iter_lines():
 
@@ -129,13 +135,15 @@ class gui(tk.Frame):
                     if userID in follewID:
                         userScName = lineFeed['user']['screen_name']
                         tweetText = lineFeed['text']
-                        contentTweet = userScName + ': '+ tweetText + '\n'
+
                         print(tweetAt)
-                        print(contentTweet)
-                        self.textBox.insert('1.0', '\n\n')
-                        self.textBox.insert('1.0', contentTweet)
-                        self.textBox.insert('1.0', '\n\n')
-                        self.textBox.insert('1.0', tweetAt)
+                        print(userScName)
+                        print(tweetText +'\n')
+                        contentTweet = codecs.encode(tweetText, 'utf-16')
+                        self.textBox.insert('2.0', '\n')
+                        self.textBox.insert('3.0', tweetAt + '\n')
+                        self.textBox.insert('4.0', userScName + '\n')
+                        self.textBox.insert('5.0', tweetText + '\n')
 
                         try:
                             winsound.Beep(900,1000)  # Beep(frequency, millisecond). The frequency parameter must be in the range 37 through 32767.
@@ -154,6 +162,8 @@ class gui(tk.Frame):
 root = tk.Tk()
 root.geometry('700x500')
 root.title('twitter stream GUI')
+root['padx'] = 20
+root['pady'] = 20
 app = gui(master=root)
 app.mainloop()
 
